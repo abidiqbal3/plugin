32f8a37fe2d94f14b0bd961111672cc0c526900f
abidiqbal3 <abid.iqbal@xgrid.co>
2022-09-28 11:12:05 +0500

    Updated formatting

--- lib/discourse_code_review/hooks.rb
+++ lib/discourse_code_review/hooks.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 module DiscourseCodeReview
-    class Hooks
+  class Hooks
       class << self
         def add_parent_category_finder(key, &blk)
           @finders ||= {}
           @finders[key] = blk
         end
-  
         def remove_parent_category_finder(key)
           @finders.delete(key)
           @finders = nil if @finders.length == 0
         end
-  
         def apply_parent_category_finder(repo_name, repo_id, issues)
           parent_category_id = nil
           if @finders
@@ -23,17 +21,14 @@ module DiscourseCodeReview
           end
           parent_category_id
         end
-  
         def add_category_namer(key, &blk)
           @namers ||= {}
           @namers[key] = blk
         end
-  
         def remove_category_namer(key)
           @namers.delete(key)
           @namers = nil if @namers.length == 0
         end
-  
         def apply_category_namer(repo_name, repo_id, issues)
           name = nil
           if @namers
@@ -45,4 +40,4 @@ module DiscourseCodeReview
         end
       end
     end
-  end
+end
